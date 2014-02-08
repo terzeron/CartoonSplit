54fd44415bf31d72717e39919a9f382762d0edc9
Young-il Cho <terzeron@gmail.com>
2014-02-08 21:09:30 +0900

    Modify the test cases

--- test/test.sh
+++ test/test.sh
@@ -18,6 +18,9 @@ diff temp.result n3.h.result > /dev/null 2>&1 || (echo "n3.h failure"; exit -1)
 ../split.py -n 2 horizontal.jpg > temp.result
 diff temp.result n2.h.result > /dev/null 2>&1 || (echo "n2.h failure"; exit -1)
 
+../split.py -n 25 -b 10 -c black vertical2.jpg > temp.result
+diff temp.result n25.v2.result > /dev/null 2>&1 || (echo "n26.v2 failure"; exit -1)
+
 echo "success"
 rm -f temp.result vertical.*.jpg horizontal.*.jpg
 
