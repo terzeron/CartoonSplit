e97c0858e14d7cf471e52b343d28bda2f816bf33
Young-il Cho <terzeron@gmail.com>
2014-02-20 16:18:32 +0900

    correct a misspelling

--- test/test.sh
+++ test/test.sh
@@ -19,8 +19,7 @@ diff temp.result n3.h.result > /dev/null 2>&1 || (echo "n3.h failure"; exit -1)
 diff temp.result n2.h.result > /dev/null 2>&1 || (echo "n2.h failure"; exit -1)
 
 ../split.py -n 25 -b 10 -c black vertical2.jpg > temp.result
-diff temp.result n25.v2.result > /dev/null 2>&1 || (echo "n26.v2 failure"; exit -1)
+diff temp.result n25.v2.result > /dev/null 2>&1 || (echo "n25.v2 failure"; exit -1)
 
 echo "success"
 rm -f temp.result vertical*.*.jpg horizontal*.*.jpg
-
